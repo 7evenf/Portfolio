d7d5ea15afd73fbce437b95384bce6b2d9dc8ca0
Vlad <vladislav.stan.1998@gmail.com>
2023-01-13 22:27:37 +0200

    added 3 sections

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 close.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__raitings-percent'),
+    lines = document.querySelectorAll('.skills__raitings-scale span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
